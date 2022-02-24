3982b73acc70feda37606f8ec42cb7b825ecc774
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-02-23 20:03:00 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1805,14 +1805,16 @@ int64_t GetBlockValue(int nHeight)
 //    cout << "GetBlockValue nHeight" << nHeight << std::endl;
 
   int64_t nSubsidy = 0;
-    if (nHeight <= 109) {
-        nSubsidy = 5000001 * COIN / 100;
-    } else if (nHeight == 110){
-        nSubsidy = 4320001 * COIN / 100;
-    } else if (nHeight <= 1500) {
-        nSubsidy = 1 * COIN / 100;
-    } else {
-        nSubsidy = 1 * COIN;
+    if (nHeight < 10) {
+        nSubsidy = 2000000 * COIN; // Premine Phase
+    } else if (nHeight <= 200000 && nHeight >= 10) {     
+        nSubsidy = 5000 * COIN; //  POW + POS
+    } else if (nHeight <= 350400 && nHeight >= 200000) {
+        nSubsidy = 2500 * COIN; // First Year
+    } else if (nHeight <= 700800 && nHeight >= 350401) {
+        nSubsidy = 1250 * COIN; // First Halving
+    } else if (nHeight >= 700801) {
+        nSubsidy = 1000 * COIN;
     }
     return nSubsidy;
 }
